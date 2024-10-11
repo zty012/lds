931aaed7b0762e663a58c0847e8fa832354bdfa6
zty012 <me@zty012.de>
2024-10-11 19:28:16 +0800

    :bug: fix deps

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import { gsap } from "gsap";
-import { ScrollToPlugin } from "gsap/ScrollToPlugin";
-import { useGSAP } from "@gsap/react";
-gsap.registerPlugin(ScrollToPlugin, useGSAP);
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
